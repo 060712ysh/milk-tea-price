3250ef8a29b744cd421ad10db235813fdac52692
060712YANG <cxks20240401@gmail.com>
2026-06-10 13:16:18 +0800

    refactor: replace Vue app initialization with Vite configuration

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-createApp(App).mount('#app')
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: '/milk-tea-price/', // ⭐ 關鍵：加上這行，兩邊都要有斜線！
+})
